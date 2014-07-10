2888f01d540e4420fb8d4832330c2819e27173a4
Sebastian Bartos <seth.kriticos@gmail.com>
2014-07-10 13:01:40 +0200

    added util.vimEdit()

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import tempfile
+import subprocess
+
 # util.drawMenu(['foo', 'bar', 'xyz'])
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~
@@ -15,4 +18,22 @@ def drawMenu(optlist):
         optlist2.append(o)
     print(' ~ '.join(optlist2))
     print(80*'~')
+
+# newcontent = util.vimEdit('foobar..')
+#
+# Edit text in vim
+# ================
+#
+# Create a temporary file, write content into it, run a subprocess with vim and
+# the file and read back the changed content.
+
+def vimEdit(content=''):
+    tmpfp = tempfile.NamedTemporaryFile()
+    tmpfilename = tmpfp.name
+    if content:
+        tmpfp.write(content.encode('UTF-8'))
+        tmpfp.flush()
+    subprocess.call(['/usr/bin/vim', tmpfilename])
+    tmpfp.seek(0)
+    return tmpfp.read().decode('UTF-8')
 
