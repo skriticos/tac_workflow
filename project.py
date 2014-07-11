79154c8228729c252b833b25caf536ad15b9e3c1
Sebastian Bartos <seth.kriticos@gmail.com>
2014-07-11 09:11:52 +0200

    added project module

---
+++ project.py
@@ -0,0 +1,13 @@
+"""
+    This module contains functions that are related to project editing.
+"""
+
+def Create(db):
+# ~~~~~~~~~~~~~
+    """
+        Create a new project. Prompt the user for name and title, let the user
+        edit the description in an external editor and send the changes to the
+        database.
+    """
+    print('Creating new project..')
+
