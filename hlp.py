1c2f7ade4a0ef5647e60e61ff08ef91aff1de5b5
Sebastian Bartos <seth.kriticos@gmail.com>
2014-07-11 09:06:57 +0200

    added hlp.RootHelp()

---
+++ hlp.py
@@ -0,0 +1,18 @@
+"""
+    Functions to output help in the various navigation menus.
+"""
+
+def RootHelp():
+# ~~~~~~~~~~~~~
+    """
+        Show help for root menu.
+    """
+    print()
+    print('QUIT:   quit application')
+    print('SELECT: select a project')
+    print('LIST:   list all projects')
+    print('TREE:   list all project trees (show all workflows)')
+    print('CREATE: create a new project')
+    print('HELP:   show this help')
+    print()
+
